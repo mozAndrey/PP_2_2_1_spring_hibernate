b75b10eda921b971be8f6181ecb931b14d66cc18
mozAndrey <3402391995aZ>
2022-05-05 12:53:21 +0300

    2.2.1 (refactored)

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDAO.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDAO.java
@@ -3,7 +3,6 @@ package hiber.dao;
 import hiber.model.Car;
 import hiber.model.User;
 
-import java.util.List;
 
 public interface CarDAO {
     void add(Car car);
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/CarService.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/CarService.java
@@ -3,8 +3,6 @@ package hiber.service;
 import hiber.model.Car;
 import hiber.model.User;
 
-import java.util.List;
-
 public interface CarService {
     void add(Car car);
 
